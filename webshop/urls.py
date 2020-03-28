0b1692cfd8f12a9eb73c864e2b494cb668be9dcc
DejanOs <deanhinich@gmail.com>
2020-03-28 19:44:33 +0000

    changed url of hompage to be the main page in url

--- webshop/urls.py
+++ webshop/urls.py
@@ -22,10 +22,11 @@ from search import urls as urls_search
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
+from homepage.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', all_products, name='index'),
+    url(r'^$', index, name='index'),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
