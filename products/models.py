19b8c89bf9363eabe534d5cafafe186a19f26561
DejanOs <deanhinich@gmail.com>
2020-03-30 15:00:10 +0000

    category model added

--- products/models.py
+++ products/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    category = (('dishwashing','Dishwashing'), ('laundry', 'Laundry'), ('cleaning', 'Cleaning'), ('disinfection', 'Disinfection'), ('body care', 'Body Care'), ('other', 'Other'))
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
+    category = models.CharField(max_length=15, choices=category, default='')
 
     def __str__(self):
         return self.name
