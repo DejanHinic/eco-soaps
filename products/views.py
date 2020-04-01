0dccc367b437d396d0ed2758322eead4ab07b4bf
DejanOs <deanhinich@gmail.com>
2020-04-01 11:23:15 +0000

    cleaning the html

--- products/views.py
+++ products/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from .models import Product
 from .models import product_image
 
-# Create your views here.
+# calling all product
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
 
-
+# calling detail 
 def details(request, id):
     details = product_image.objects.filter(product_id = id)
     product = Product.objects.get(id=id)
